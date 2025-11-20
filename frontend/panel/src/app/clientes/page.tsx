032a2ef05dfaf42001452b4fe18875d0566d449e
collejas <collejas1@gmail.com>
2025-11-20 20:02:38 +0000

    YA TENGO VISTA DE CLIENTES, FALTA ORDENARLA

--- frontend/panel/src/app/clientes/page.tsx
+++ frontend/panel/src/app/clientes/page.tsx
@@ -1,6 +1,4 @@
 import { AppViewLayout } from "@/components/layouts/app-view-layout";
-import { SectionCards } from "@/components/section-cards";
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { loadClientesData } from "@/lib/clientes/data";
 
@@ -11,10 +9,6 @@ export default async function ClientesPage() {
 
   return (
     <AppViewLayout title="Clientes">
-      <SectionCards data={data.cards} />
-      <div className="px-4 lg:px-6">
-        <ChartAreaInteractive data={data.chart} />
-      </div>
       <DataTable data={data.table} />
     </AppViewLayout>
   );
